85d00d9acd7e729dbc812534454fd19923dda9f7
Tatyaniya <afromyak13@gmail.com>
2021-04-09 20:34:31 +0300

    deploy.sh

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/Todos-Vue/',
     filename: 'build.js'
   },
   module: {
